7c9e7fa505f5a2c91ca53d9ba0aa265efab4c115
brianjrok <brianjrok@gmail.com>
2022-02-19 15:52:49 -0500

    set up local storage

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -1,1 +1,1 @@
-console.log("Hello World");
+!function(e){var o;try{o=window.localStorage;var t="__storage_test__";return o.setItem(t,t),o.removeItem(t),!0}catch(e){return e instanceof DOMException&&(22===e.code||1014===e.code||"QuotaExceededError"===e.name||"NS_ERROR_DOM_QUOTA_REACHED"===e.name)&&o&&0!==o.length}}()?console.log("No local world!"):console.log("Hello, World!");
